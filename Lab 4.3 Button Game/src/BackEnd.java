6d00ef1db3809a1fa7e46c9e3bf506519c5c838a
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2018-03-27 09:22:00 -0400

    g

--- Lab 4.3 Button Game/src/BackEnd.java
+++ Lab 4.3 Button Game/src/BackEnd.java
@@ -1,4 +1,25 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+	
 public class BackEnd {
-
+	public static void main(String[] args) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(new File("highScores.csv")));
+			System.out.println("File succesfully read! :)\n");
+			in.append("3");
+			String line = in.readLine();
+			while (line != null) {
+				String[] scores = line.split(",");
+				for (String num : scores) {
+					System.out.println(num);
+				}
+				line = in.readLine();
+			}
+		}
+		catch (IOException ee) {
+			System.out.println("IO EXCEPTION !! :(");
+		}
+	}
 }
